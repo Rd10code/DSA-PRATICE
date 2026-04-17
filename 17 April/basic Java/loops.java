6da6475c9c8276e97f0b66aba054b5e587d1db1b
Rohit Deore <deorerohit2004@gmail.com>
2026-04-17 16:03:24 +0530

    Done with the Loops

---
+++ 17 April/basic Java/loops.java
@@ -0,0 +1,26 @@
+public class loops {
+    public static void main(String[] args) {
+
+        // while loop
+        // int day = 0;
+        // while(day>=-12){
+        //     System.out.println(day);
+        //     day--;
+        // }
+
+        //For loop
+        // for(int i=0;i<=1000;i++){
+        //     System.out.println(i);
+        // }
+
+        //DO While loops + typecasting
+
+        // int day =(int)1.4;
+        // do{
+        //     System.out.print(day);
+        //     day++;
+        // }while(day>2);
+ 
+}
+}
+          
