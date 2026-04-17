7af839aba9bee4d6bd3655b9a5e2504b98342df0
Rohit Deore <deorerohit2004@gmail.com>
2026-04-17 14:55:25 +0530

    Done withe varibles & datatypes

---
+++ 17 April/basic Java/basics.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class basics {
+    public static void main(String[] args) {
+        //Input Taking
+
+        Scanner sc= new Scanner(System.in);
+        // System.out.print("Enter the Radius of the Circle ");
+        // int num = sc.nextInt();
+        // System.out.println(num*num*3.14);
+        // System.out.print(num+2);
+
+        //Type Casting
+        // float a = 24.10f;
+        // int b = (int)a;
+        // System.out.println(b);
+        // sc.close();
+
+        
+    }    
+}
